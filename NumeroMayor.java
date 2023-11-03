d24962127c0eadf9b9bff8f93eed52cd54516a3c
Joaquin Dominguez <jdominguezsantana@gmail.com>
2023-11-03 08:22:58 +0100

    cambio a los tres números

--- NumeroMayor.java
+++ NumeroMayor.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class NumeroMayor {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        System.out.println("Introduzca tres números: ");
+        System.out.println("Introduzca los tres números: ");
         Integer num1 = reader.nextInt();
         Integer num2 = reader.nextInt();
         Integer num3 = reader.nextInt();
